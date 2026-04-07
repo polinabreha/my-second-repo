5c0a919b3a24e2a8306b0cbfbce6b581c8640649
polinabreha <polinabreha@gmail.com>
2026-04-07 11:12:57 -0400

    Added the tree message

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -5,5 +5,14 @@ public class App {
         System.out.println("Testing to see if it works");
         System.out.println("A little nice message");
         System.out.println("One more");
+        System.out.println("Tree:)");
+        String a = "  *";
+        String b = " ***";
+        String c = "*****";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+
     }
 }
