95ba6339407d8700d18a543380d5ba129f9a317e
polinabreha <polinabreha@gmail.com>
2026-04-07 10:59:36 -0400

    Added 2 messages

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -3,5 +3,7 @@ package com.pluralsight;
 public class App {
     public static void main(String[] args) {
         System.out.println("Testing to see if it works");
+        System.out.println("A little nice message");
+        System.out.println("One more");
     }
 }
